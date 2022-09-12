bf1139ffbc05077532b2a4022047f00ee6dfc3d7
UP210878 <UP210878@alumnos.upa.edu.mx>
2022-09-12 10:48:55 -0500

    dslgsdl

---
+++ U1 Programming Introduction/01_Hola.cpp
@@ -0,0 +1,22 @@
+/*
+Date: 12/09/2022 
+Author: VVM UP210878
+Email: UP210878@alumnos.upa.edu.mx
+Description: My first program in C++
+Last Modification: -
+*/
+
+//------------ PREPROCESSOR DIRECTIVES ----------------
+
+//imput/output library between the screen and keyboard
+#include <iostream> //Library
+
+//------------------ MAIN FUNCTION ---------------------
+
+int main(){ //Main function
+    using namespace std;
+    cout << "Hello World! How ya doing?:)";//Print on screen a text
+    
+    return 0;//Return 0 is to see that the program is working and that it returns a whole value.
+}
+
