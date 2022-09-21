383ab3be6068df4999c4f2d9d9b4063855e15a30
UP210878 <UP210878@alumnos.upa.edu.mx>
2022-09-21 10:23:07 -0500

    Practica

--- U1 Programming Introduction/03_EntradadeDatos.cpp
+++ U1 Programming Introduction/03_EntradadeDatos.cpp
@@ -37,24 +37,24 @@ int main() {
     cin >> caracter;
     cout << "El dato ingresado es: " << caracter << endl;
 
-    printf("\n===========================\n\n"); //Usando printf y scanf (Wakala)
+    printf("\n\n===========================\n\n"); //Usando printf y scanf (Wakala)
 
     printf("Este programa muestra el ingreso de datos por el usuario. \n");
     printf("Ingresa un dato de tipo entero: ");
     scanf("%d", &entero);
-    printf("El dato ingresado es:  %d \n", entero);
+    printf("El dato ingresado es:  %d \n\n", entero);
 
     printf("Ingresa un dato de tipo flotante: ");
     scanf("%f", &flotante);
-    printf("El dato ingresado es:  %f \n", flotante);
+    printf("El dato ingresado es:  %f \n\n", flotante);
 
     printf("Ingresa un dato de tipo double: ");
     scanf("%lf", &grande);
-    printf("El dato ingresado es:  %lf \n", grande);
+    printf("El dato ingresado es:  %lf \n\n", grande);
 
     printf("Ingresa un dato de tipo caracter: ");
     scanf("%s", &caracter);
-    printf("El dato ingresado es:  %c \n", caracter);
+    printf("El dato ingresado es:  %c \n\n", caracter);
 
     return 0;
 }
