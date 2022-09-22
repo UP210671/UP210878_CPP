ff831337f30095602f9708468ff8021656d80bc6
UP210878 <UP210878@alumnos.upa.edu.mx>
2022-09-22 10:41:46 -0500

    01

--- U2 Control Structures and Cycles/01_Renta.cpp
+++ U2 Control Structures and Cycles/01_Renta.cpp
@@ -9,10 +9,33 @@ Last Modification: 22/09/2022
 //------------ PREPROCESSOR DIRECTIVES ----------------
 
 #include <iostream>
+
 using namespace std;
+
+//---------------------- ISSUE -------------------------
+
+/*
+Los tramos impositivos para la declaración de la renta en un determinado país son los siguientes:
+
+Renta	Tipo impositivo
+Menos de $10,000	5%
+Entre $10,000 y $20,000	15%
+Entre $20,000 y $35,000	20%
+Entre $35,000 y $60,000	30%
+Más de $60,000	45%
+Escribir un programa que pregunte al usuario su renta anual y muestre por 
+pantalla el tipo impositivo que le corresponde.
+*/
 
 //------------------ MAIN FUNCTION ---------------------
 
-int main (){
-    
+int main(){
+    int Renta, Descuento;
+    cout << "Ingrese su pago de renta";
+    cin >> Renta;
+    if(Renta>=60000){
+        Descuento=Renta*.45
+    }
+
+    return 0;
 }
