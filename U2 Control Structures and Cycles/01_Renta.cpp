aa26df915e404caa06c9c75a50ccb930909e19e8
UP210878 <UP210878@alumnos.upa.edu.mx>
2022-09-22 10:29:53 -0500

    Exercise1

---
+++ U2 Control Structures and Cycles/01_Renta.cpp
@@ -0,0 +1,18 @@
+/*
+Date: 22/09/2022 
+Author: Vinc3nt (Figured its better than using an ID)
+Email: UP210878@alumnos.upa.edu.mx
+Description: Rent discounts.
+Last Modification: 22/09/2022
+*/
+
+//------------ PREPROCESSOR DIRECTIVES ----------------
+
+#include <iostream>
+using namespace std;
+
+//------------------ MAIN FUNCTION ---------------------
+
+int main (){
+    
+}
